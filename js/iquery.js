86a707d7151bc99937ad30fb3a440ed56de93da6
Chaooohs <01nikka01@gmail.com>
2022-11-17 05:55:46 +0200

    arrange

--- js/iquery.js
+++ js/iquery.js
@@ -1,12 +1,14 @@
 
 
-$('#nav').prepend($('#input'));
+
 
 $(document).ready(function() {
   $('.burger-btn').click(function(event){
     $('.burger-btn,.nav').toggleClass('active');
     $('body').toggleClass('lock');
     
+    $('#input').show();
+    $('#nav').prepend($('#input'));
     $('#about').clone().appendTo('#nav');
     $('#contact').clone().appendTo('#nav');
     $('#partner').clone().appendTo('#nav');
