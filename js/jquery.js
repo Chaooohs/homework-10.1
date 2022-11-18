a89e2984262ef4102b3fc6f2cbfc142a9ab29099
Chaooohs <01nikka01@gmail.com>
2022-11-18 18:53:35 +0200

    added scrollto

--- js/jquery.js
+++ js/jquery.js
@@ -1,19 +1,29 @@
 
 
-$(document).ready(function() {
-  $('.mob-button').click(function(event){
+$(document).ready(function () {
+  $('.mob-button').click(function (event) {
     $('.mob-button, .mob-logo, .mob-input, .mob-nav, .nav').toggleClass('active');
-    
+
     $('body').toggleClass('lock');
 
     if ($('.mob-btn-img').attr('src') == 'img/button/close.svg')
       $('.mob-btn-img').attr('src', 'img/burger.svg').toggleClass('asd');
     else
       $('.mob-btn-img').attr('src', 'img/button/close.svg').toggleClass('asd');
-    
+
     // $('.mob-input').show();
     // $('.nav').show();
-    
+
     $('.nav').appendTo('.mob-nav');
+  });
+});
+
+
+jQuery(document).ready(function () {
+  jQuery("a.scrollto").click(function () {
+    elementClick = jQuery(this).attr("href")
+    destination = jQuery(elementClick).offset().top - 200;
+    jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 500);
+    return false;
   });
 });
