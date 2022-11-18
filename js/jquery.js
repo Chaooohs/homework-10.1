bdfec3e21e4bba03ef3e093fed73f2317c7ca899
Chaooohs <01nikka01@gmail.com>
2022-11-18 08:17:35 +0200

    added close button

--- js/jquery.js
+++ js/jquery.js
@@ -4,14 +4,15 @@ $(document).ready(function() {
   $('.mob-button').click(function(event){
     $('.mob-button, .mob-logo, .mob-input, .mob-nav, .nav').toggleClass('active');
     $('body').toggleClass('lock');
+
+    if ($('.mob-btn-img').attr('src') == 'img/button/close.svg')
+      $('.mob-btn-img').attr('src', 'img/burger.svg');
+    else
+      $('.mob-btn-img').attr('src', 'img/button/close.svg');
     
     // $('.mob-input').show();
     // $('.nav').show();
     
     $('.nav').appendTo('.mob-nav');
-    
-    // $('.from').afterTo('.nav-list');
-    // $('#contact').clone().appendTo('.nav-list');
-    // $('#partner').clone().appendTo('.nav-list');
   });
 });
