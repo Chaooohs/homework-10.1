27dfbcc710da3e1e9253108b25bcb31120041b50
Chaooohs <01nikka01@gmail.com>
2022-11-20 14:51:02 +0200

    coments

--- js/jquery.js
+++ js/jquery.js
@@ -4,26 +4,31 @@ $(document).ready(function () {
   $('.mob-button').click(function (event) {
     $('.mob-button, .mob-logo, .mob-input, .mob-nav, .nav').toggleClass('active');
 
+// включает класс lock на body для отключения скрола во время открытия меню бургер    
     $('body').toggleClass('lock');
-
+    
+// меняет изо бургер на крестик и обратно
     if ($('.mob-btn-img').attr('src') == 'img/button/close.svg')
       $('.mob-btn-img').attr('src', 'img/button/burger.svg').toggleClass('asd');
     else
       $('.mob-btn-img').attr('src', 'img/button/close.svg').toggleClass('asd');
-
+// включат блок
     // $('.mob-input').show();
     // $('.nav').show();
 
-    $('.nav').appendTo('.mob-nav');
+//переносит меню nav в меню бургер
+    $('.nav').appendTo('.mob-nav');       
   });
 });
 
 
+// скрол при переходе между # заглушками
 $(document).ready(function () {
-  $("a.scrollto").click(function () {
+  $("a.scrollto").click(function () {               
     elementClick = jQuery(this).attr("href")
-    destination = jQuery(elementClick).offset().top - 270;
+    destination = jQuery(elementClick).offset().top - 270; // растояние к верху экрана
     $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 500);
+    // время
     return false;
   });
 });
