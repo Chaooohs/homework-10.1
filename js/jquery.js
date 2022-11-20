a4e150eedc503e1245f2fdae1a36dbb5def4014d
Chaooohs <01nikka01@gmail.com>
2022-11-20 17:56:32 +0200

    arrange

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,44 @@
 
 
+// скрол при переходе между # заглушками
+
+$(document).ready(function () {
+  $("a.scrollto").click(function () {
+    elementClick = jQuery(this).attr("href")
+    destination = jQuery(elementClick).offset().top - 270; // растояние к верху экрана
+    $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 500);
+    // время
+    return false;
+  });
+});
+
+
+
+// перенос блока nav при break-point 993px
+
+var $blocksContainer1 = $('.content'),
+  $blocksContainer2 = $('.mob-nav');
+
+function checkBlocks() {
+  $('.nav').each(function () {
+    var $movingBlock = $(this);
+
+    if ($(window).width() < 993) {
+      $movingBlock.appendTo($blocksContainer2);       // последним
+    } else {
+      $movingBlock.prependTo($blocksContainer1);      // первым
+    }
+  });
+}
+
+$(window).on('resize', function () {
+  checkBlocks();
+});
+
+
+
+// открытие меню при нажатии на бургер
+
 $(document).ready(function () {
   $('.mob-button').click(function (event) {
     $('.mob-button, .mob-logo, .mob-input, .mob-nav, .nav').toggleClass('active');
@@ -23,37 +62,11 @@ $(document).ready(function () {
 })
 
 
-// скрол при переходе между # заглушками
-$(document).ready(function () {
-  $("a.scrollto").click(function () {
-    elementClick = jQuery(this).attr("href")
-    destination = jQuery(elementClick).offset().top - 270; // растояние к верху экрана
-    $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 500);
-    // время
-    return false;
-  });
-});
 
 
 
-var $blocksContainer1 = $('.content'),
-  $blocksContainer2 = $('.mob-nav');
 
-function checkBlocks() {
-  $('.nav').each(function () {
-    var $movingBlock = $(this);
 
-    if ($(window).width() < 993) {
-      $movingBlock.appendTo($blocksContainer2);       // последним
-    } else {
-      $movingBlock.prependTo($blocksContainer1);      // первым
-    }
-  });
-}
-
-$(window).on('resize', function () {
-  checkBlocks();
-});
 
 
 
